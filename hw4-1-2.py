a5a414ea6a27e27683502cde03e3820844b62174
Alex Joliet <p21ajoliet@fairfieldprep.org>
2020-10-07 14:16:09 -0400

    HW 2, 3 coded

--- hw4-1-2.py
+++ hw4-1-2.py
@@ -1,4 +1,15 @@
-#Create a program that determines if you are safe or bust in blackjack. 
-# Prompt the user for the value of their 2 cards and display if they are safe or busted. 
-# Note: Jack, Queen, and King are all worth 10, 
-# and if the sum of your cards is under 21, then you are safe.
+# Author: ALJ (AMDG) 10/7/20
+
+#Only input the number of cards drawn, input zero for cards not drawn
+
+card1 = int(input("What is the value of your first card? "))
+card2 = int(input("What is the value of your second card? "))
+card3 = int(input("What is the value of your third card? "))
+card4 = int(input("What is the value of your fourth card? "))
+card5 = int(input("WHat is the value of your fifth card? "))
+card_value = (card1 + card2 + card3 + card4 + card5)
+
+if card_value > 21:
+    print("You bust!")
+else:
+    print("You're safe.")
--- hw4-1-3.py
+++ hw4-1-3.py
@@ -1,2 +1,20 @@
-#Write a program that tests if a given number is a multiple of 2, 3, or 5. 
-#Display the result of whether it is divisible by each number on a separate line.
+# Author: ALJ (AMDG) 10/7/20
+
+x = int(input("What number are you thinking of? "))
+
+if x % 2 == 0 and x % 3 == 0 and x % 5 == 0:
+    print("The number you are thinking of is divisable by two, three, and five.")
+elif x % 2 == 0 and x % 3 == 0:
+    print("The number you are thinking of is divisable by two and three, but not divisable by five.")
+elif x % 2 == 0 and x % 5 == 0:
+    print("The number you are thinking of is divisable by two and five, but not divisable by three.")
+elif x % 3 == 0 and x % 5 == 0:
+    print("The number you are thinking of is divisable by three and five, but not divisable by two.")
+elif x % 2 == 0:
+    print("The number you are thinking of is divisable by two, but not divisable by three and five.")
+elif x % 3 == 0:
+    print("The number you are thinking of is divisable by three, but not divisable by two and five.")
+elif x % 5 == 0:
+    print("The number you are thinking of is divisable by five, but not divisable by two and three.")
+else:
+    print("The number you are thinking of is not divisable by two, three, or five.")
