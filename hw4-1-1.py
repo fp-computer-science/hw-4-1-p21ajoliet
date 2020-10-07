eefdf4cdf24d8562400711857cbae46cacb06204
Alex Joliet <p21ajoliet@fairfieldprep.org>
2020-10-07 13:29:41 -0400

    HW 4-1 coded

--- hw4-1-1.py
+++ hw4-1-1.py
@@ -1,5 +1,19 @@
-#Author
-#In the group stage of the FIFA World Cup, a team earns 3 points for a win, 1 point for a tie, 
-# and no points for a loss. Write a program that propmts the user to input the number of wins and ties for 
-# 2 teams, calculates the total points each team earned in the group stage, and displays a statement stating 
-# which team finished with more points during the group stage. Be sure to use if and/or if-else statements.
+# Author: ALJ (AMDG) 10/6/20
+
+wins = 3
+ties = 1
+
+team1_wins = int(input("How many wins did Team One have? "))
+team1_ties = int(input("How many ties did Team One have? "))
+team1_points = (team1_wins * wins + team1_ties * ties)
+
+team2_wins = int(input("How many wins did Team Two have? "))
+team2_ties = int(input("How many ties did Team Two have? "))
+team2_points = (team2_wins * wins + team2_ties * ties)
+
+if team1_points > team2_points:
+    print("Team One beat Team Two!")
+elif team2_points > team1_points:
+    print("Team Two beat Team One!")
+else:
+    print("Team One tied Team Two!")
