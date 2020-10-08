0743950cf938708c826fea0965cde89af44950cf
Alex Joliet <p21ajoliet@fairfieldprep.org>
2020-10-07 20:22:47 -0400

    proofread

--- hw4-1-2.py
+++ hw4-1-2.py
@@ -6,7 +6,7 @@ card1 = int(input("What is the value of your first card? "))
 card2 = int(input("What is the value of your second card? "))
 card3 = int(input("What is the value of your third card? "))
 card4 = int(input("What is the value of your fourth card? "))
-card5 = int(input("WHat is the value of your fifth card? "))
+card5 = int(input("What is the value of your fifth card? "))
 card_value = (card1 + card2 + card3 + card4 + card5)
 
 if card_value > 21:
